0742a4d2ecb08966e68c31bb0395b08b38111530
Stijn-Flipper <w.stijn@gmail.com>
2014-08-31 22:16:49 +0200

    implemented mathjax plugin

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -16,11 +16,35 @@ namespace Phile\Plugin\Flipper\MathJax;
 class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
 
   /**
+   * MathJax configuration settins, see
+   *
+   * @var array mathjax_settings
+   */
+  private $mathjax_settings;
+
+  /**
+   * Mathjax prefix for meta.
+   *
+   * @var string mathjax_prefix
+   */
+  private $mathjax_prefix;
+
+  /**
    * Constructor.
    *
    * Register plugin to Phile Core.
    */
   public function __construct() {
+    \Phile\Event::registerEvent('after_parse_content', $this);
+
+    // default plugin configurations
+    $this->mathjax_settings = array(
+      'enabled' => false,
+      'config'  => 'default',
+      'version' => 'latest',
+    );
+
+    $this->mathjax_prefix = 'mathjax_';
     return;
   }
 
@@ -33,7 +57,33 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
    * @return void
    */
   public function on($event, $data=null) {
-    return;
+    // get current page
+    $page = $data['page'];
+    if (null === $page)
+      return;
+
+    // get meta prefixed with the given mathjax prefix
+    $meta = array();
+    foreach ($page->getMeta()->getAll() as $key => $value) {
+      if (0 !== strpos($key, $this->mathjax_prefix))
+        continue;
+
+      $meta[substr($key, strlen($this->mathjax_prefix))] = $value;
+    } // end foreach
+
+    $this->mathjax_settings = array_merge($this->mathjax_settings, $this->settings, $meta);
+
+    // get twig variables
+    $key = 'templateVars';
+    $twig_vars = (\Phile\Registry::isRegistered($key)) ? \Phile\Registry::get($key) : array();
+
+    // add mathjax twig variable
+    $enabled = $this->mathjax_settings['enabled'];
+    $enabled = (is_bool($enabled)) ? $enabled : (0 == strlen($enabled));
+
+    $twig_vars['mathjax'] = ($enabled) ? sprintf('<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/%s/MathJax.js?config=%s"></script>',
+                                                 $this->mathjax_settings['version'], $this->mathjax_settings['config']) : '';
+    \Phile\Registry::set($key, $twig_vars);
   }
 
 }
--- config.php
+++ config.php
@@ -4,4 +4,7 @@
  * Configuration file for phileMathJax plugin.
  */
 return array(
+  'enabled' => false,
+  'version' => 'latest',
+  'config'  => 'default',
 );
